44a5eb039dc6a588975b17f055c6fd4acc796677
Gottuso <583185@bah.com>
2018-01-23 16:48:58 -0500

    Minor cosmetic touch ups

--- gg_gauge.R
+++ gg_gauge.R
@@ -11,9 +11,9 @@ gg.gauge <- function(pos,custom_label,breaks=c(0,30,70,100)) {
     return(data.frame(x,y))
   }
   ggplot()+ 
-    geom_polygon(data=get.poly(breaks[1],breaks[2]),aes(x,y),fill="red")+
-    geom_polygon(data=get.poly(breaks[2],breaks[3]),aes(x,y),fill="yellow")+
-    geom_polygon(data=get.poly(breaks[3],breaks[4]),aes(x,y),fill="green")+
+    geom_polygon(data=get.poly(breaks[1],breaks[2]),aes(x,y),fill="darkred")+
+    geom_polygon(data=get.poly(breaks[2],breaks[3]),aes(x,y),fill="darkgoldenrod2")+
+    geom_polygon(data=get.poly(breaks[3],breaks[4]),aes(x,y),fill="darkgreen")+
     geom_polygon(data=get.poly(pos-1,pos+1,0.2),aes(x,y))+
     geom_text(data=as.data.frame(breaks), size=3, fontface="bold", vjust=0,
               aes(x=1.1*cos(pi*(1-breaks/100)),y=1.1*sin(pi*(1-breaks/100)),label=paste0(breaks,"%")))+
